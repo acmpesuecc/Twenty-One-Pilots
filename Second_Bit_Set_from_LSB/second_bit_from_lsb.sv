f2ee9127b22e8ac3ef29746f5e330ea0468f3dce
anudeepak772 <anudeepak567@gmail.com>
2025-10-17 23:08:23 +0530

    Refactor Second_Bit_Set_from_LSB module with comments
    Refactor the module to improve readability and add comments explaining the logic for obtaining the first and second bits.

--- Second_Bit_Set_from_LSB/second_bit_from_lsb.sv
+++ Second_Bit_Set_from_LSB/second_bit_from_lsb.sv
@@ -5,12 +5,13 @@ module Second_Bit_Set_from_LSB #(
     output wire [WIDTH-1:0] second_bit_o
 );
 
-    wire [WIDTH-1:0] first_bit;
-    assign first_bit = vec_i & ((~vec_i) + 1);
+    wire [WIDTH-1:0] first_bit; 
+    assign first_bit = vec_i & ((~vec_i) + 1);// obtaining the first bit ( all th 0's become 1 so when adding 1, all ones are flipped except the first 1 which becomes 0 which then becomes 1)
 
     wire [WIDTH-1:0] first_cleared;
-    assign first_cleared = (~first_bit) & vec_i;
+    assign first_cleared = (~first_bit) & vec_i; //clearing the first bit from the vector
 
-    assign second_bit_o = first_cleared & ((~first_cleared) + 1);
+    assign second_bit_o = first_cleared & ((~first_cleared) + 1); // obtaining the second bit
+
 
 endmodule
